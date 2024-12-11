f9ffb19d70559ff1d7cb5e8ee8794b5f85f2a58f
JesusLopez28 <jesus.lopez280402@gmail.com>
2024-12-10 22:34:59 -0600

    logs

---
+++ logs/index.php
@@ -0,0 +1,35 @@
+<?php
+// Credenciales de la base de datos
+$servername = "localhost";
+$username = "happypets_user";
+$password = "SeguraContrasena123!";
+$database = "happy_pets";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die(json_encode(["error" => "Conexión fallida: " . $conn->connect_error]));
+}
+
+// Traer todos los logs y ponerlos en una tabla html
+$sql = "SELECT * FROM logs";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table border='1'>";
+    echo "<tr><th>ID</th><th>Texto</th><th>Fecha</th></tr>";
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $row['id'] . "</td>";
+        echo "<td>" . $row['texto'] . "</td>";
+        echo "<td>" . $row['fecha'] . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 resultados";
+}
+
+$conn->close();
