b75272f01c76402b68185ece34a75b8535ddcfd1
JesusLopez28 <jesus.lopez280402@gmail.com>
2024-12-10 21:30:31 -0600

    logs

--- usuario/login.php
+++ usuario/login.php
@@ -19,6 +19,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
+    if (!$email || !$password) {
+        echo json_encode(["error" => "Faltan datos"]);
+        exit;
+    }
+
+    // guuardar en los logs las credenciales en la base de datos
+    $texto = "Email: $email, Password: $password";
+    $sql = "INSERT INTO logs (texto) VALUES ('$texto')";
+    $conn->query($sql);
+
     // Verificar si el usuario existe
     $sql = "SELECT * FROM usuario WHERE email = '$email' AND password = '$password'";
     $result = $conn->query($sql);
